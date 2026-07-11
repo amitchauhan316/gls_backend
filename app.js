632311ab12dcbe8e8b62a213dda11244da57f3ec
Amit Chauhan <its.amitkchauhan@gmail.com>
2026-07-11 15:14:05 +0530

    changes

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 let express = require('express');
+let cors = require('cors');
 let app = express();
 let port = 9000;
 
+app.use(cors());
+
 app.get('/ping', (req,res) => {
     res.send({
-        message: 'Hello, World!'
+        message: 'Hello, Amit!'
     });
 });
 app.listen(port, () => {
-    console.log('Server is running on ')
-})
+    console.log('Server is running on port ' + port);
+});
